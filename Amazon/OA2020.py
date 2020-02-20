85ed7a361b5e9884a598e09c97f6de75fd617b1d
Fei <50836371+Fei@users.noreply.github.com>
2020-02-19 19:50:16 -0600

    Update OA2020.py

--- Amazon/OA2020.py
+++ Amazon/OA2020.py
@@ -1,10 +1,10 @@
 
 #Common Algorithms
 import numpy as np
-from collections import deque
+from collections import deque,defaultdict
 from copy import copy,deepcopy
 import math
-
+#%%
 '''
 2.Tresure Island I --DEQUE
 A map [['O','O','O','O'],
@@ -137,10 +137,59 @@ def roll_dice(x):
         count[i-1]+=1
     roll=math.inf
     for i in x:
-        nroll=sum(count)-count[i-1]+count[6-i]
+        nroll=sum(count)-count[i-1]+count[6-i]#多考虑数字之间的联系，有的时候不一定用数据结构
         if roll>nroll:
             roll=nroll
     return roll
+
+
+'''
+6.Min Cost to Connect All Nodes --Minimum Spanning Tree
+Given an undirected graph with n nodes labeled 1..n. Some of the nodes are already connected. The i-th edge connects nodes edges[i][0] and edges[i][1] together. Your task is to augment this set of edges with additional edges to connect all the nodes. Find the minimum cost to add new edges between the nodes such that all the nodes are accessible from each other.
+
+Input:
+
+n, an int representing the total number of nodes.
+edges, a list of integer pair representing the nodes already connected by an edge.
+newEdges, a list where each element is a triplet representing the pair of nodes between which an edge can be added and the cost of addition, respectively (e.g. [1, 2, 5] means to add an edge between node 1 and 2, the cost would be 5).
+Example 1:
+
+Input: n = 6, edges = [[1, 4], [4, 5], [2, 3]], newEdges = [[1, 2, 5], [1, 3, 10], [1, 6, 2], [5, 6, 5]]
+Output: 7
+Explanation:
+There are 3 connected components [1, 4, 5], [2, 3] and [6].
+We can connect these components into a single component by connecting node 1 to node 2 and node 1 to node 6 at a minimum cost of 5 + 2 = 7.
+'''
+def connect_nodes(e,ne,n):
+    #Tuple() is unchangable set is {}
+    uf= {}#dic
+    #create union find for the initial edges given
+    def find(edge):
+        uf.setdefault(edge,edge) #if the edge not exit, insert "edge" with the value "edge"
+        if uf[edge]!=edge:
+            uf[edge]=find(uf[edge])#find the loop
+        return uf[edge]
+    
+    def union(edge1,edge2):
+        uf[find(edge1)]=find(edge2)
+        
+    for e1,e2 in e:
+        if find(e1)!=find(e2):
+            union(e1,e2)
+            
+    #sort the new edges by cost
+    #if an edge is not part of the minimum spanning tree, then include it, else continue
+    Tcost=0
+    for v1,v2,cost in sorted(ne, key=lambda x:x[2]):
+        if find(v1)!=find(v2):
+            union(v1,v2)
+            Tcost+=cost
+            
+    if len({find(c) for c in uf})==1 and len(uf)==n:
+        return Tcost
+    else:
+        return -1
+
 
 #%%
 if __name__=='__main__':
@@ -173,3 +222,8 @@ if __name__=='__main__':
         a=[1, 6, 2, 3]
         b=[1,1,2,3,4,5,6,1,2,3]
         print(roll_dice(b))
+    if i =='6':
+        n = 6
+        edges = [[1, 4], [4, 5], [2, 3]]
+        new_edges = [[1, 2, 5], [1, 3, 10], [1, 6, 2], [5, 6, 5]]
+        print(connect_nodes(edges,new_edges,n))
